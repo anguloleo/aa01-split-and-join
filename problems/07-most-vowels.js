155811b373d0389f02182e76219e9db032e2377d
anguloleo <anguloleo@gmail.com>
2024-09-13 17:53:22 -0700

    split and join completed

--- problems/07-most-vowels.js
+++ problems/07-most-vowels.js
@@ -6,26 +6,47 @@ Vowels are the letters "a", "e", "i", "o", "u".
 
 function mostVowels(sentence){
 
+    // sentence turned into array
     let arr = sentence.split(" ");
+
     let word = "";
     let vowels = "aeiou";
-    let count = 0;
+    let count;
     let wordMostVowels;
- 
+    let highestCount = 0;
 
+    if(sentence.length < 1){
+        return sentence;
+    }
+
+    else{
+ 
+    //cycle through words in array
     for(let i = 0; i < arr.length; i++){
 
         word = arr[i];
-        wordMostVowels = arr[i];
-        
+        count = 0;
 
+        //count number of vowels in word
         for(let j = 0; j < word.length; j++){
+
             if(vowels.includes(word[j])){
                 count++;
             }
         }
 
-        if(count)
+        // assign word with most vowels
+        if(count > highestCount){
+            highestCount = count;
+            wordMostVowels = word;
+        }
+        else{
+            highestCount = highestCount;
+            wordMostVowels = wordMostVowels;
+        }
+    }
+}
+return wordMostVowels;
 }
 
 // console.log(mostVowels("what a wonderful life")); // "wonderful"
